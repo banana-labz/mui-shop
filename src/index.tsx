785f069ebadfe88e6ab7187825dabc828f600bfc
therion-666 <strike201420141@gmail.com>
2022-04-11 00:23:03 +0300

    added mui and moved localstorage operations

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
-import data from "./data.json"
-
 import { render } from "react-dom"
 import { Provider } from "react-redux"
 import { App } from "./components/app"
 import { store } from "./store"
-
-if (!localStorage.getItem("items"))
-    localStorage.setItem("items", JSON.stringify(data))
 
 render(
     <Provider store={store}>
--- src/services/shop-service.ts
+++ src/services/shop-service.ts
@@ -1,10 +1,18 @@
 import type { ProductData } from "../types"
 
+import data from "../data.json"
+
+const STORAGE_SECTION = "items"
+
+if (!localStorage.getItem(STORAGE_SECTION)) {
+    localStorage.setItem(STORAGE_SECTION, JSON.stringify(data))
+}
+
 class ShopService {
     public getItems = (): Promise<ProductData[]> => (
         new Promise((resolve, reject) => {
             try {
-                const json = localStorage.getItem("items")
+                const json = localStorage.getItem(STORAGE_SECTION)
                 if (json) {
                     resolve(JSON.parse(json))
                 }
