fbf9d5a7f8c4e18df87d8354f2c8b6ca7ea38cb0
therion-666 <strike201420141@gmail.com>
2022-04-11 19:40:27 +0300

    added icon to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Badge, IconButton, Toolbar, Typography } from "@mui/material"
 
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
+import LoginIcon from "@mui/icons-material/Login"
 
 export const Header = () => (
   <AppBar position="static">
@@ -8,6 +9,9 @@ export const Header = () => (
       <Typography component="span" sx={{ flexGrow: 1 }}>
         MUI Shop
       </Typography>
+      <IconButton color="inherit">
+        <LoginIcon/>
+      </IconButton>
       <IconButton color="inherit">
         <Badge color="secondary" badgeContent={1}>
           <ShoppingCartIcon/>
